ad18bc4922410ffde4f938a0145c4bf0ecf774e9
Ruochen Chen <ruocchen1220@gmail.com>
2026-07-27 09:37:41 +0800

    更新心意按钮提示文案

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const promise = document.querySelector('#promiseButton');
 const secret = document.querySelector('#secret');
 promise.addEventListener('click', () => {
   secret.classList.toggle('show');
-  promise.innerHTML = secret.classList.contains('show') ? '我爱你 ♥' : '点一下，收下我的心意 <b>♥</b>';
+  promise.innerHTML = secret.classList.contains('show') ? '我爱你 ♥' : '点一下这里就给你摸鸡';
 });
 
 const player = document.querySelector('#bgm');
